1ab66eff721a26cf7303626b861d44b53684b8e5
Olesya Lesechko <76412011+Kenobichek@users.noreply.github.com>
2021-12-03 23:47:31 +0200

    Update main.cpp

--- FactoryMethod/main.cpp
+++ FactoryMethod/main.cpp
@@ -1,3 +1,14 @@
+
+// Фабричный метод.
+// Написать программу, которая
+//   - получает на вход полный путь к папке
+//   - собирает список всех файлов в этой папке (можно нерекурсивно), а также их
+//     свойства (размер/ время создания, модификации, доступа/ архивный, только для
+//     чтения и т.д.), список свойств на Ваш выбор, минимум 2 помимо имени файла.
+//   - выводит «отчет» по содержимому папки в файл в формате txt, csv, xml, json –
+//     любые 2 или больше на Ваш выбор.
+// Используйте фабричный метод.
+
 #include <iostream>
 #include <memory>
 #include <windows.h>
@@ -14,12 +25,10 @@ int main() {
 	fs::path path;
 
 	std::wcin >> path;
-
 	
 	std::unique_ptr<TxtCreator> txt_creator = std::make_unique<TxtCreator>();
 	std::unique_ptr<JsonCreator> json_creator = std::make_unique<JsonCreator>();
 	
-
 	txt_creator->parseFileDir(path);
 	txt_creator->factoryReports(path);
 	
